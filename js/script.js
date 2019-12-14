2e120bc8786e21ab6b3842099f87adc3b19cbb62
buitravel <buitravel@example.net>
2019-12-14 11:36:11 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+﻿$(function(){
+  var places = [
+    { value: 'Hà Nội', data: 'HN' },
+    { value: 'TP Hồ Chí Minh', data: 'HCM' },
+    { value: 'Đà Lạt', data: 'DL' },
+    { value: 'Sa Pa', data: 'SP' },
+    { value: 'Đà Nẵng', data: 'DN' },
+    { value: 'Hội An', data: 'HA' },
+    
+  ];
+  
+  // setup autocomplete function pulling from currencies[] array
+  $('#autocomplete').autocomplete({
+    lookup: places,
+    onSelect: function (suggestion) {
+      var thehtml = '<strong>Currency Name:</strong> ' + suggestion.value + ' <br> <strong>Symbol:</strong> ' + suggestion.data;
+      $('#outputcontent').html(thehtml);
+    }
+  });
+  
+
+});
